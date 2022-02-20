d3dcdd0c26680e9bff6d823c41789a24c52fca63
gt012@uark.edu <98195311+nootnoot3k@users.noreply.github.com>
2022-02-19 21:22:18 -0600

    fixed db?

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, flash
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-# from flask import SQLAlchemy 
+from flask import Flask, render_template, request, flash\
+try:
+	from flask_sqlalchemy import SQLAlchemy
+except:
+	print("couldnt get the flask_sql stuff")
 # from flask_sqlalchemy import SQLAlchemy
 
 # -- sample program from this video <https://youtu.be/6plVs_ytIH8>
@@ -13,8 +14,8 @@ app.secret_key = "manbearpig_MUDMAN888" #required for flask to operate
 # ENV = 'dev'
 
 # if ENV == 'dev':
-# 	app.debug = True
-# 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lezbitgtjkbfrs:aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d2gpgbag2bgopb
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lezbitgtjkbfrs:aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d2gpgbag2bgopb
 # else:
 # 	app.debug = False
 # 	app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -22,27 +23,21 @@ app.secret_key = "manbearpig_MUDMAN888" #required for flask to operate
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
+db = SQLAlchemy(app)#connect app to database?
 
-#added some database stuff?
-
-engine = "postgres://lezbitgtjkbfrs:aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d2gpgbag2bgopb"
-db = scoped_session(sessionmaker(bind=engine))
-db.execute("INSERT INTO codename VALUES (1,1,1,1)")
-db.commit()
-
-# db = SQLAlchemy(app)#connect app to database
-
-# class Player(db.Model):
-# 	__tablename__ = 'player'
-# 	id = db.Column(db.Integer)
-# 	first_name = db.Column(db.String(30))
-# 	last_name  = db.Column(db.String(30))
-# 	codename   = db.Column(db.String(30))
+class Player(db.Model):
+	__tablename__ = 'player'
+	id = db.Column(db.Integer)
+	first_name = db.Column(db.String(30))
+	last_name  = db.Column(db.String(30))
+	codename   = db.Column(db.String(30))
 	
-# 	def __init__(self, first_name, last_name, codename):
-# 		self.first_name = first_name
-# 		self.last_name  = last_name
-# 		self.codename   = codename
+	def __init__(self, first_name, last_name, codename):
+		self.first_name = first_name
+		self.last_name  = last_name
+		self.codename   = codename
+	def __repr__(self):
+		return f "dang"
 	
 # @app.route("/")#allows for us to change something when a user uses one of our inputs
 # def index():
@@ -58,6 +53,7 @@ def playerEntry2():
 def edit():
 	flash("hi " + str(request.form["player_input"]))
 	id = str(request.form["player_input"])
+	print(id)
 	return render_template("playerEntry2.html")
 	
 
