933cba48dbbcc23492e60a6f6e7a6d40524f1621
gt012@uark.edu <98195311+nootnoot3k@users.noreply.github.com>
2022-02-19 20:24:52 -0600

    changed the import problem

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, flash
-from flask import flask_sqlalchemy import SQLAlchemy
+from flask import flask_sqlalchemy 
+from flask_sqlalchemy import SQLAlchemy
 
-#sample program from this video <https://youtu.be/6plVs_ytIH8>
-#our specific code was created by Matt and james.
+# -- sample program from this video <https://youtu.be/6plVs_ytIH8>
+#  --specific code was created by Matt and james.
 
 app = Flask(__name__)#makes a class for the app or program we wish to run
 app.secret_key = "manbearpig_MUDMAN888" #required for flask to operate
@@ -32,26 +33,26 @@ class Player(db.Model):
 		self.last_name  = last_name
 		self.codename   = codename
 	
-@app.route("/")#allows for us to change something when a user uses one of our inputs
-def index():
-	flash("Hello World")
-	return render_template("index.html")
+# @app.route("/")#allows for us to change something when a user uses one of our inputs
+# def index():
+# 	flash("Hello World")
+# 	return render_template("index.html")
 
 @app.route("/edit") #player entry route to the player entry form
 def playerEntry():
 	return render_template("playerEntry2.html")
 
-# @app.route('/submit', methods=['POST'])#route to submit form,sends information to the database due to POST 
-# def submit():
-# 	if request.method == 'POST':
-# 		id = request.form['id'] #request form gets the data stored 
-# 		first_name = request.form['first_name']
-# 		last_name = request.form['last_name']
-# 		codename = request.form['codename']
+@app.route('/submit', methods=['POST'])#route to submit form,sends information to the database due to POST 
+def submit():
+	if request.method == 'POST':
+		id = request.form['id'] #request form gets the data stored 
+		first_name = request.form['first_name']
+		last_name = request.form['last_name']
+		codename = request.form['codename']
 		
-# 		if db.session.query(Player).filter(Player.id == id).count() == 0:
-# 			data = Player(id, first_name, last_name, codename)
-# 			db.session.add(data)
-# 			db.session.commit()
-# 			--return render_template('success.html')
-# 		return render_template('index.html')
+		if db.session.query(Player).filter(Player.id == id).count() == 0:
+			data = Player(id, first_name, last_name, codename)
+			db.session.add(data)
+			db.session.commit()
+			return render_template('success.html')
+		return render_template('index.html')
