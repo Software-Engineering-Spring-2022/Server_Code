c77a102ca2cf51ebc80702601164b62b82f54ae3
Matthew-Clemence <98196571+Matthew-Clemence@users.noreply.github.com>
2022-02-21 12:40:04 -0600

    Adding connection to Databse

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, flash
 import os
 import playerEnt
+import psycopg2
 try:
 	from flask_sqlalchemy import SQLAlchemy
 except:
 	os.system("pip install flask_sqlalchemy") #trying to install dependencies if failed
 	from flask_sqlalchemy import SQLAlchemy
-
 
 # -- sample program from this video <https://youtu.be/6plVs_ytIH8>
 #  --specific code was created by Matt and james.
@@ -14,18 +14,27 @@ except:
 app = Flask(__name__)#makes a class for the app or program we wish to run
 app.secret_key = "manbearpig_MUDMAN888" #required for flask to operate
 
+conn = psycopg2.connect(
+	host="ec2-34-206-148-196.compute-1.amazonaws.com"
+	database="d2gpgbag2bgopb"
+	user="lezbitgtjkbfrs"
+	password="aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8"
+)
+	
 # ENV = 'dev'
 
 # if ENV == 'dev':
-app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lezbitgtjkbfrs:aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d2gpgbag2bgopb'
+# app.debug = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lezbitgtjkbfrs:aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d2gpgbag2bgopb'
 # else:
 # 	app.debug = False
 # 	app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)#connect app to database?
+# db = SQLAlchemy(app)#connect app to database?
+
+
 
 # class Player(db.Model):
 # 	__tablename__ = 'player'
