3fdb79227b6cfcd51f2c590f88fd862735b1d375
gt012@uark.edu <98195311+nootnoot3k@users.noreply.github.com>
2022-02-20 22:33:23 -0600

    yote

--- playerEnt.py
+++ playerEnt.py
@@ -18,7 +18,7 @@ class playerEnt:
         
 
     def plyr_sc1(self):
-        app = self.app
+        app = Flask(self.app)
         print("in method plyr_scr1")
         @app.route("/playerEntry2", methods = ["POST", "GET"]) #player entry route to the player entry form in the html
         def playerEntry2():
