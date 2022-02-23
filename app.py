1d7f510541bfbebc24bf3d7fee9c4596732e7ee9
Matthew-Clemence <98196571+Matthew-Clemence@users.noreply.github.com>
2022-02-22 22:05:47 -0600

    Added Config function to simplify database connection

--- app.py
+++ app.py
@@ -13,16 +13,36 @@ app = Flask(__name__)#makes a class for the app or program we wish to run
 app.secret_key = "manbearpig_MUDMAN888" #required for flask to operate
 i = 0
 
-def insert_player(ID, FIRST_NAME, LAST_NAME, CODENAME):	# Call this to insert players into the database table player
+def config():
 	conn = None
 	try:
 		conn = psycopg2-binary.connect( # connects to database
-			user="lezbitgtjkbfrs",
-			password="aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8",
-			host="ec2-34-206-148-196.compute-1.amazonaws.com",
-			port="5432",
-			database="d2gpgbag2bgopb")
+				user="lezbitgtjkbfrs",
+				password="aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8",
+				host="ec2-34-206-148-196.compute-1.amazonaws.com",
+				port="5432",
+				database="d2gpgbag2bgopb")
+	except (Exception, psycopg2-binary.DatabseError) as error:
+			print(error)
+	finally:
+			if conn is not None:
+					conn.close()
+					
+	return conn
 
+def insert_player(ID, FIRST_NAME, LAST_NAME, CODENAME):	# Call this to insert players into the database table player
+	conn = None
+	try:
+#		conn = psycopg2-binary.connect( # connects to database
+#			user="lezbitgtjkbfrs",
+#			password="aa6fa77497eff9cdf22c8d618ab6277c8df71e537b9c2e46237fd3901277f7f8",
+#			host="ec2-34-206-148-196.compute-1.amazonaws.com",
+#			port="5432",
+#			database="d2gpgbag2bgopb")
+		params = config()
+	
+		conn = psycopg2-binary.connect(**params)
+		
 		cur = conn.cursor() # creating cursor object
 
 		""" insert a new player int players table """
@@ -73,4 +93,4 @@ def splash():
 
 if __name__ == "__main__":
 	app.run(debug=True)
-	insert_player(1, "Jelly", "Fish", "WaterBoi")
+	insert_player(2, "Big", "Boi", "Hulk")
