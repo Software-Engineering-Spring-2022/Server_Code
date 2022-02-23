6bbad5209dcefb7bb9f2bd1133d6c70c2769239b
gt012@uark.edu <98195311+nootnoot3k@users.noreply.github.com>
2022-02-23 15:15:22 -0600

    cast to string?

--- app.py
+++ app.py
@@ -65,13 +65,9 @@ def edit():
 		last_name = data.getlist("player_last")
 		#using try catch in case the program breaks
 		try:
-			# print(iD[1])
-			# print(iD[0])
-			# print(first_name)
-			# print(last_name)
-			# print(codename)
+			
 			for x in len(iD): #there always be as many ID's as there are players
-				insert_player(iD[x],first_name[x],last_name[x],codename[x]) #calls the DB insert player method
+				insert_player(str(iD[x]),str(first_name[x]),str(last_name[x]),str(codename[x])) #calls the DB insert player method
 				print(x)#error checking
 			
 		except:
