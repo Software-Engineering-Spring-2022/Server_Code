87788d72f1ef48593b48aec88ae4e3ca38b2750b
gt012@uark.edu <98195311+nootnoot3k@users.noreply.github.com>
2022-02-23 15:16:14 -0600

    text check

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ def edit():
 		last_name = data.getlist("player_last")
 		#using try catch in case the program breaks
 		try:
+			print(len(iD))
 			
 			for x in len(iD): #there always be as many ID's as there are players
 				insert_player(str(iD[x]),str(first_name[x]),str(last_name[x]),str(codename[x])) #calls the DB insert player method
