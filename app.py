78ce899c9eee23fcaee14f9a90a315c6b2fa1a80
gt012@uark.edu <98195311+nootnoot3k@users.noreply.github.com>
2022-03-11 14:43:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def plyr_scrn():
 		print(data)
 
 
-	return render_template("actionScreen.html", plyr_id = ((data_test[d]) for d in data_test), plyr_codename = "yep")
+	return render_template("actionScreen.html", data = data)
 
 
 def create_dict(key,values):
