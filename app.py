9570e1da32475e1aa2eab1c0bc5f230a4d09decc
Matthew-Clemence <98196571+Matthew-Clemence@users.noreply.github.com>
2022-04-05 13:41:52 -0500

    Added client code
    This app is the client and the server is separate. The traffic generator should send events to the server, then the client(app) receives them. I'm not sure where to run the client and traffic generator code yet or where to start the server.

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ app = Flask(__name__)#makes a class for the app or program we wish to run
 app.secret_key = "manbearpig_MUDMAN888" #required for flask to operate
 i = 0
 
+"""
+msgFromClient       = "Hello UDP Server"
+bytesToSend         = str.encode(msgFromClient)
+serverAddressPort   = ("127.0.0.1", 20001)
+bufferSize          = 1024
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
+"""
+
 def insert_player(ID, FIRST_NAME, LAST_NAME, CODENAME):	# Call this to insert players into the database table player
 	conn = None
 	try:
@@ -195,52 +213,6 @@ def plyr_scrn():
 		 	blue_team = ["no players entered"]
 
 		events = ["opus hit HOBBES", "missy hit calvin", "razor hit bill_the_cat"]	
-
-	""" Traffic Generator
-	bufferSize  = 1024
-	serverAddressPort   = ("127.0.0.1", 7501)
-
-
-	print('this program will generate some test traffic for 2 players on the red ')
-	print('team as well as 2 players on the blue team')
-	print('')
-
-	red1 = input('Enter id of red player 1 ==> ')
-	red2 = input('Enter id of red player 2 ==> ')
-	blue1 = input('Enter id of blue player 1 ==> ')
-	blue2 = input('Enter id of blue player 2 ==> ')
-
-	print('')
-	counter = input('How many events do you want ==> ')
-
-	# Create datagram socket
-	UDPClientSocketTransmit = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-	# counter number of events, random player and order
-	i = 1
-	while i < int(counter):
-		if random.randint(1,2) == 1:
-			redplayer = red1
-		else:
-			redplayer = red2
-
-		if random.randint(1,2) == 1:
-			blueplayer = blue1
-		else: 
-			blueplayer = blue2	
-
-		if random.randint(1,2) == 1:
-			message = redplayer + ":" + blueplayer
-		else:
-			message = blueplayer + ":" + blueplayer
-
-		print(message)
-		i+=1;
-		UDPClientSocketTransmit.sendto(str.encode(str(message)), serverAddressPort)
-		time.sleep(random.randint(1,3))
-
-	print("program complete")
-	"""
 	
 	return render_template("actionScreen.html", red_team = red_team,blue_team = blue_team,events = events)
 
