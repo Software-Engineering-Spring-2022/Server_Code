846a7b3f6ff1fe4fa39f31ceca26401632abad63
JESwai <98196003+JESwai@users.noreply.github.com>
2022-04-08 17:14:33 -0500

    added traffic generator to app.py

--- app.py
+++ app.py
@@ -92,12 +92,70 @@ def listen_to_udp():
 		r = select.select([UDPServerSocket])
 		for i in r:
 			events.append(i.recvfrom(buffersize))
+			
+			
+			
+			
+@celery.task()
+def traffic_generator():
+	bufferSize  = 1024
+	serverAddressPort   = ("127.0.0.1", 7501)
+
+
+	print('this program will generate some test traffic for 2 players on the red ')
+	print('team as well as 2 players on the blue team')
+	print('')
+
+	# red1 = input('Enter codename of red player 1 ==> ')
+	# red2 = input('Enter codename of red player 2 ==> ')
+	# blue1 = input('Enter codename of blue player 1 ==> ')
+	# blue2 = input('Enter codename of blue player 2 ==> ')
+	
+	red1 = "John"
+	red2 = "James"
+	blue1 = "Matthew"
+	blue2 = "Ryan"
+
+	print('')
+	# counter = input('How many events do you want ==> ')
+	counter = 10
+	# Create datagram socket
+	UDPClientSocketTransmit = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+	# counter number of events, random player and order
+	i = 1
+	while i < int(counter):
+		if random.randint(1,2) == 1:
+			redplayer = red1
+		else:
+			redplayer = red2
+
+		if random.randint(1,2) == 1:
+			blueplayer = blue1
+		else: 
+			blueplayer = blue2	
+
+		if random.randint(1,2) == 1:
+			message = redplayer + " hit " + blueplayer
+		else:
+			message = blueplayer + " hit " + redplayer
+
+		print(message)
+		i+=1;
+		UDPClientSocketTransmit.sendto(str.encode(str(message)), serverAddressPort)
+		time.sleep(random.randint(1,3))
+		
+	print("program complete")
+
+
 	
 #Splash screen (default) route. Redirect to player entry screen after initializing components
 @app.route("/")#allows for us to change something when a user uses one of our inputs
 def splash():
 	t1 = threading.Thread(target = listen_to_udp)
 	t1.start()
+	t2 = threading.Thread(target = listen_to_udp)
+	t2.start()
 #	listen_to_udp.delay()
 	print("UDP server up and listening")
 	
@@ -261,6 +319,7 @@ def plyr_scrn():
 		blue_team = ["no players entered"]
 		
 	return render_template("actionScreen.html", red_team = red_team,blue_team = blue_team,events = events)
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
