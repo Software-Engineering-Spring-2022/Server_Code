80b288ccd950de53a2f89404a9b4831d166c989b
JESwai <98196003+JESwai@users.noreply.github.com>
2022-04-10 20:47:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def listen_to_udp():
 		events[2]=events[1]
 		events[1]=events[0]
 		events[0]=msg
-		# turbo.push(turbo.replace(app.render_template('events.html',events = events), 'EVENT'))
+		turbo.push(turbo.replace(app.render_template('events.html',events = events), 'EVENT'))
 		# turbo.push(turbo.replace(render_template('red_team.html',red_team = session.get('red_team',list)), 'RED'))
 		# turbo.push(turbo.replace(render_template('blue_team.html',blue_team = session.get('blue_team',list)), 'BLUE'))
 
