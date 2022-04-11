ebc825dc5f100a4727637adfcb89b70483ea080e
James Swaim <98428822+J-Swaim@users.noreply.github.com>
2022-04-10 23:08:56 -0500

    Fixed Error

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def listen_to_udp():
 						RedPlayerNames.append("")
 				else:
 					RedPlayerNames.append(playerInfo)
-			flash = !flash
+			flash = not flash
 		
 		#Update the array of events with the new event
 		events[4]=events[3]
